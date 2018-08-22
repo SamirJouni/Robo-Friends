60cce09ce2a1ab9f6fbd99a17737c69e387c4d8e
F1r3byte <f1r3byte@yandex.com>
2018-08-22 12:19:46 +0200

    created a card component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Card = ()=> {
+	return (
+		<div>
+			<img src='' />
+			<div>
+				<h2>John Doe</h2>
+				<p>johndoe@jmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
