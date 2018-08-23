8958255fd3604c084f5c846973c232ce29909a9e
F1r3byte <f1r3byte@yandex.com>
2018-08-23 20:23:22 +0200

    added feature to empty and refill placeholder on focus and blur

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,16 @@ import React from 'react';
 
 const SearchBox = ({searchfield, searchChangeState}) => {
 
+	const placeholder = 'Search Robots';
+
 	return (
 		<div className='pa2 '>
 			<input
 			className='pa3 ba b--green bg-lightest-blue'
 			type='search'
-			placeholder='Search Robots'
+			placeholder={placeholder}
+			onFocus={(event)=> event.target.placeholder=''}
+			onBlur={(event)=> event.target.placeholder= placeholder}
 			onChange={searchChangeState}/>
 		</div>
 	);
