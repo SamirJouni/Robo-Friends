eae34b5caf3f8730dc77120097d16276b50f7764
F1r3byte <f1r3byte@yandex.com>
2018-08-23 17:21:41 +0200

    cleaned up the code

--- src/App.js
+++ src/App.js
@@ -16,14 +16,9 @@ class App extends Component {
 	}
 
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users').then(
-
-		response => {
-			return response.json();
-
-		}).then(users=> {
-			this.setState({robots: users})
-		});
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users=> this.setState({robots: users}));
 	}
 
 
