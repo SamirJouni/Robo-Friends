d75680c2df8289cd51141887780ab2168768e846
F1r3byte <f1r3byte@yandex.com>
2018-08-23 17:16:38 +0200

    receive robots from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 
@@ -11,10 +10,22 @@ class App extends Component {
 		super ();
 
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: '',
 		}
 	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then(
+
+		response => {
+			return response.json();
+
+		}).then(users=> {
+			this.setState({robots: users})
+		});
+	}
+
 
 	onSearchChangeState = (event) => {
 
