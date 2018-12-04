55c8eb56c411ea18b90b908e2164c3c767330fd4
F1r3byte <f1r3byte@yandex.com>
2018-12-04 23:43:23 +0100

    Updates

---
+++ precache-manifest.c6ec4e3cb228ddef192d8f611c455513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52bed6483f4ce2cd8d49",
+    "url": "/Robo-Friends/static/css/main.8bd60c8e.chunk.css"
+  },
+  {
+    "revision": "52bed6483f4ce2cd8d49",
+    "url": "/Robo-Friends/static/js/main.52bed648.chunk.js"
+  },
+  {
+    "revision": "86a729cca80d5f3a6f9f",
+    "url": "/Robo-Friends/static/css/1.1938dbf1.chunk.css"
+  },
+  {
+    "revision": "86a729cca80d5f3a6f9f",
+    "url": "/Robo-Friends/static/js/1.86a729cc.chunk.js"
+  },
+  {
+    "revision": "c8b8b6a36eb3e702f1e3",
+    "url": "/Robo-Friends/static/js/runtime~main.c8b8b6a3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "52bf35bccf6dfb5bff66afa2b67466bd",
+    "url": "/Robo-Friends/index.html"
+  }
+];
